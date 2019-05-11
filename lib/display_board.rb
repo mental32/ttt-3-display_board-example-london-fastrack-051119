c8d7d365d66ace72406c3d69db15c09f5aef877d
beatific-revision-5216 <ivanov_chris@yahoo.com>
2019-05-11 10:26:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def display_board
-  for i in 0..3 do
+  for i in 0..4 do
     puts (i % 2 == 0) ? "   |   |   " : "-----------"
   end
 end
